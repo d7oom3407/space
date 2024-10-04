bafd3e4e706a441ab9d5eb0a0e8e61726664edf9
d7oom3407 <116514120+d7oom3407@users.noreply.github.com>
2024-10-04 20:45:48 +0300

    Add files via upload

---
+++ src/Header.js
@@ -0,0 +1,16 @@
+// src/components/Header.js
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header>
+            {
+                <h3>
+                    Shop now in Smart Way!
+                </h3>
+            }
+        </header>
+    );
+};
+
+export default Header;
---
+++ src/data.js
@@ -0,0 +1,113 @@
+/* 
+https://www.iconfinder.com/icons/1243689/call_phone_icon
+Creative Commons (Attribution 3.0 Unported);
+https://www.iconfinder.com/Makoto_msk */
+
+export const storeProducts = [
+  {
+    id: 1,
+    title: "Fridge",
+    img: "img/product-1.jpg",
+    price: 2000,
+    company: "LG",
+    info:
+      "The freezer is located at the bottom, allowing easier access to fresh food.",
+    count: 0,
+    total: 0
+  },
+  {
+    id: 2,
+    title: "Samsung S7",
+    img: "img/product-2.jpg",
+    price: 1000,
+    company: "LG",
+    info:
+      "Designed to fit in tight spaces, such as dorm rooms, offices, or small kitchens.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 3,
+    title: "Oven",
+    img: "img/product-3.jpg",
+    price: 8,
+    company: "Samsung",
+    info: "Use electromagnetic radiation to heat food quickly.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 4,
+    title: "Oven",
+    img: "img/product-4.jpg",
+    price: 18,
+    company: "Haam",
+    info:
+      "Deal for toasting, baking, broiling, and reheating.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 5,
+    title: "AC",
+    img: "img/product-5.jpg",
+    price: 24,
+    company: "LG",
+    info:
+      "Cools an entire building through a system of ducts.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 6,
+    title: "AC",
+    img: "img/product-6.jpg",
+    price: 17,
+    company: "LG",
+    info: "Operates quietly and maintains consistent temperatures",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 7,
+    title: "Washing Machine",
+    img: "img/product-7.jpg",
+    price: 30,
+    company: "LG",
+    info:
+      "Loading and unloading is straightforward, requiring less bending.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 8,
+    title: "Wasging Machine",
+    img: "img/product-8.jpg",
+    price: 2,
+    company: "Samsung",
+    info:
+      "Use a tumbling action that is gentler on clothes, reducing wear and tear.",
+    inCart: false,
+    count: 0,
+    total: 0
+  }
+];
+
+export const detailProduct = {
+  id: 1,
+  title: "Google Pixel - Black",
+  img: "img/product-1.png",
+  price: 10,
+  company: "google",
+  info:
+    "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+  inCart: false,
+  count: 0,
+  total: 0
+};
